b3eba0e4c153710eb9dce5a306e94755c8b60063
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-12 09:34:13 +0900

    [0512] 실습9 추가

--- app4-1.js
+++ app4-1.js
@@ -3,6 +3,7 @@
 // express 객체 생성
 var express = require('express');
 var http = require('http');
+var fs = require('fs');
 var app = express();
 
 app.use(function(req, res, next) {
@@ -44,9 +45,15 @@ app.use(function(req, res, next) {
     // res.writeHead('200', {'Content-Type':'text/html; charset=utf8'});
     // res.end('<h2>Express 서버에서 ' + req.user + '를 res, writeHead와 end로 응답한 결과입니다. </h2>');
 
-    // 실습8 / 됨 
-    req.user = 'ebaeg'
-    res.send('<h2>Express 서버에서 ' + req.user + '를 send로 응답한 결과입니다. </h2>');
+    // // 실습8 / 됨 
+    // req.user = 'ebaeg'
+    // res.send('<h2>Express 서버에서 ' + req.user + '를 send로 응답한 결과입니다. </h2>');
+
+    // 실습9
+    var filename = 'house.png';
+    fs.readFile(filename, function(err, data) {
+        res.send(data);
+    });
 });
 
 
