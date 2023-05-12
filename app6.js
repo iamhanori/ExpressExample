f62b56baac94f3ba43ea482d56f9658dbd0f6cc2
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-12 09:56:35 +0900

    [0512] 헤더와 요청 파라미터 확인

---
+++ app6.js
@@ -0,0 +1,25 @@
+// 0512 헤더와 요청 파라미터 확인
+
+// express 객체 생성
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.use(function(req, res, next) {
+    console.log('첫번째 미들웨어에서 요청을 처리함.');
+
+    var userAgent = req.header('User-Agent');
+    var paramName = req.query.name;
+
+    res.writeHead('200', {'Content-Type':'text/html; charset=utf8'});
+    res.write('<h2>Express 서버에서 응답한 결과입니다. </h2>');
+    res.write('<div<p>User-Agent :'+ userAgent + '</p></div>');
+    res.write('<div<p>Param Name :'+ paramName + '</p></div>');
+    res.end();
+});
+
+// expres server 시작
+// http://localhost:3000/?name=sunny
+http.createServer(app).listen(3000, function() {
+    console.log('express server start. : 3000');
+});
