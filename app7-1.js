6e0ed303cc0484d3cc4e31c21e21e4961c36ef05
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-24 10:19:00 +0900

    app7-1.js res.redirect('/');

---
+++ app7-1.js
@@ -0,0 +1,39 @@
+// 0517 app.use('/', static(path.join(__dirname, 'public'))); 수정
+
+// 모듈 설치
+var express = require('express');
+var http = require('http');
+var static = require('serve-static');
+
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// express 객체 생성
+var app = express();
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({extended:false}));
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json());
+
+app.use('/', static(path.join(__dirname, 'public')));
+// app.use(static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+    console.log('첫번째 미들웨어에서 요청을 처리함.');
+
+    res.redirect('/');
+});
+
+
+// expres server 시작
+// http://localhost:3000?id=test&password=1234
+// http://localhost:3000/image/girl.png
+// http://localhost:3000/public/stylesheets/style.css
+http.createServer(app).listen(3000, function() {
+    console.log('express server start. : 3000');
+});
