8ca0de6eca937208a0f6f52236098c6b1a336fe1
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-26 08:59:13 +0900

    URL 파라미터 사용하기

---
+++ app8-2.js
@@ -0,0 +1,46 @@
+let express = require('express');
+let path = require('path');
+let http = require('http');
+let static = require('serve-static');
+
+let bodyParser = require('body-parser');
+
+// 익스프레스 객체 생성
+let app = express();
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({extended:false}))
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json());
+
+
+// app.use('/public', static(path.join(__dirname, 'public')));
+app.use('/public', static(path.join(__dirname, 'public')));
+
+var router = express.Router();
+router.route('/process/login/:name').post(function(req, res) {
+    console.log('/process/login/:name 처리함.');
+
+    var paramName = req.params.name;
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+    res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.write('<div><p>Param name : ' + paramName + '</p></div>');
+    res.write('<div><p>Param id : ' + paramId + '</p></div>');
+    res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
+    res.write("<br><br><a href='/public/login3.html'>로그인 페이지로 돌아가기</a>");
+    res.end();
+});
+
+app.use('/', router);
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+})
